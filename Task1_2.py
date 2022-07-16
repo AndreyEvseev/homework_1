264b1f5b6eae63e466bf162229ef0608e7ab4043
Andrey Evseev <a-evseev@yandex.ru>
2022-07-16 12:57:31 +0300

    Решение задач 1 и 2

--- Task1_2.py
+++ Task1_2.py
@@ -1,3 +1,9 @@
-# Задание 2. Напишите программу для. проверки истинности утверждения 
+# Задание 2. Напишите программу дляпроверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
+identity = True
+for x in True, False:
+    for y in True, False:
+        for z in True, False:
+            print(f'{x = } {y = } {z = }         result: {not(x or y or z) == (not x and not y and not z)}')
+print(f'{identity = }')
