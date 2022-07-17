ec39aecf1258d53d7f370313e20c50e8dd97bb22
Andrey Evseev <a-evseev@yandex.ru>
2022-07-17 18:03:36 +0300

    Решение задания 8

--- Task1_8.py
+++ Task1_8.py
@@ -1,2 +1,15 @@
 # Задание 8. Найти наибольший простой делитель числа
 
+n = int(input('Введите натуральное число N: '))
+simple_divisor = 1
+for i in range(2, n):
+    if n % i == 0:
+        j = 2
+        while i % j != 0 or j == n - 1:
+            j += 1
+        if j == i:
+            simple_divisor = i
+    else:
+        continue
+print(f'Наибольший простой делитель числа {n} = {simple_divisor};')    
+
